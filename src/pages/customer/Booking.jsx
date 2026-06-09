f6320a148d8c7be475cc6b00a86fed1ed70f4310
pham dong chinh <dchinh6803@gmail.com>
2026-06-09 20:19:41 +0700

    ..

--- src/pages/customer/Booking.jsx
+++ src/pages/customer/Booking.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useParams, useNavigate, useLocation } from "react-router-dom";
 import { createNotification } from "../../api/notification";
 import { loadStripe } from "@stripe/stripe-js";
